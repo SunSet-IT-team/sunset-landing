8ca4fef1f901bde5c0c7c5f69c80afa454a9c2c4
Николай Сердученко <nikserduchenko@yandex.ru>
2024-11-20 19:31:15 +0300

    chore: configure tailwind (fonts, colors)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'var(--background)',
+				foreground: 'var(--foreground)',
+				blue: 'var(--blue)',
+				grey: 'var(--grey)',
+				orange: 'var(--orange)',
+			},
+			fontFamily: {
+				akony: 'var(--font-akony)',
+				'arodora-pro': 'var(--font-arodora-pro)',
+			},
+			backgroundImage: {
+				'black-gradient': 'var(--black-gradient)',
+			},
+		},
+	},
+	plugins: [],
+} satisfies Config
