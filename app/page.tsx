06a86fa43dff57ece0637faa7b3d69657b717f3d
Николай Сердученко <nikserduchenko@yandex.ru>
2024-11-25 22:41:30 +0300

    feat: add section animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-	return <div className=''></div>
+import Section from './shared/components/Sections/Section'
+import Container from './shared/components/ui/Container'
+import { sections } from './shared/data/data'
+
+export default function Page() {
+	return (
+		<Container className='relative overflow-hidden h-full -mt-40 pt-40'>
+			{sections.map(section => (
+				<Section sectionId={section.id} key={section.id + section.text}>
+					<div className='font-akony'>{section.content}</div>
+				</Section>
+			))}
+		</Container>
+	)
 }
---
+++ app/shared/components/Sections/Services/Services.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const Services: FC = () => {
+	return <div>Услуги</div>
+}
+
+export default Services
