a20002f399d9ae6f2fee5418aebebcc511eb9079
Николай Сердученко <nikserduchenko@yandex.ru>
2024-12-28 13:03:45 +0300

    feat: add not found page

---
+++ app/(system-pages)/[...not-found]/page.tsx
@@ -0,0 +1,25 @@
+import { CubesModel } from '@/app/components/Models/CubesModel'
+import { Metadata } from 'next'
+import { FC } from 'react'
+
+export const metadata: Metadata = {
+	title: 'Страница не найдена',
+	description: 'Что-то пошло не так, страница не найдена',
+}
+
+const Custom404: FC = () => {
+	return (
+		<>
+			<div className='flex flex-col justify-center items-center h-full relative z-10'>
+				<p className='text-7xl'>404</p>
+				<p className='font-arodora-pro'>Что-то пошло не так!</p>
+			</div>
+			<div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full opacity-55'>
+				{/* <div className='absolute w-full h-full left-0 right-0 top-0 bottom-0 bg-black z-20 bg-opacity-55'></div> */}
+				<CubesModel />
+			</div>
+		</>
+	)
+}
+
+export default Custom404
