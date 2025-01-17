d59681c7d465df777d7e49aef7275a78478ddec5
pan0mera <artess.ponomarev@mail.ru>
2025-01-17 12:57:37 +0300

    Исправление ошибки при сборке

--- app/components/Contacts/validationSchema.ts
+++ app/components/Contacts/validationSchema.ts
@@ -1,27 +1,27 @@
-import validator from 'validator'
-import { z, ZodType } from 'zod'
-import { IContactData } from './contactData.type'
+import validator from 'validator';
+import { z, ZodType } from 'zod';
+import { IContactData } from './contactData.type';
 
 export const contactSchema: ZodType<IContactData> = z.object({
-	name: z
-		.string({
-			required_error: 'Имя обязательно',
-		})
-		.min(1, 'Имя обязательно')
-		.regex(/^[а-яА-Яa-zA-Z]+$/, 'Имя должно содержать только буквы'),
-	email: z
-		.string({
-			required_error: 'Email обязателен',
-		})
-		.min(1, 'Email обязателен')
-		.email('Введите корректный email'),
-	phone: z
-		.string({
-			required_error: 'Телефон обязателен',
-		})
-		.min(1, 'Телефон обязателен')
-		.refine(validator.isMobilePhone, {
-			message: 'Введите корректный номер телефона',
-		}),
-	message: z.string().optional(),
-})
+    name: z
+        .string({
+            required_error: 'Имя обязательно',
+        })
+        .min(1, 'Имя обязательно')
+        .regex(/^[а-яА-Яa-zA-Z]+$/, 'Имя должно содержать только буквы'),
+    email: z
+        .string({
+            required_error: 'Email обязателен',
+        })
+        .min(1, 'Email обязателен')
+        .email('Введите корректный email'),
+    phone: z
+        .string({
+            required_error: 'Телефон обязателен',
+        })
+        .min(1, 'Телефон обязателен')
+        .refine((value) => validator.isMobilePhone(value), {
+            message: 'Введите корректный номер телефона',
+        }),
+    message: z.string().optional(),
+}) as ZodType<IContactData>;
