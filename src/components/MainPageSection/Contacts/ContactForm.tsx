5c3f9d87b6be601ede146989eeb667737f94cb2e
At1set <iliya9874329@gmail.com>
2025-08-02 13:37:17 +0300

    Правки по форме контактов
    feat: опустил Textarea "готовы обсудить проек" в самый низ формы.
    
    feat: изменил label у Textarea на "Пару слов о Вашем проекте".
    
    feat: добавил для поля с вводом телефона маску.
    
    Добавление анимации растягивания Textarea на всю высоту в процессе...

--- src/components/MainPageSection/Contacts/ContactForm.tsx
+++ src/components/MainPageSection/Contacts/ContactForm.tsx
@@ -1,160 +1,164 @@
-'use client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { AnimatePresence, motion } from 'framer-motion';
-import { FC, useState } from 'react';
-import { Controller, useForm } from 'react-hook-form';
-import { IContactData } from './contactData.type';
-import FormSuccess from './FormSuccess';
-import Lines from './Lines';
-import { contactSchema } from './validationSchema';
-import { metrika, MetrikGoal } from '@/src/feature/metrika/MetrikSender';
-import Field from '../../ui/Field';
-import Textarea from '../../ui/Textarea';
-import Button from '../../ui/Button';
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { AnimatePresence, motion } from "framer-motion"
+import { FC, useState } from "react"
+import { Controller, useForm } from "react-hook-form"
+import { IContactData } from "./contactData.type"
+import FormSuccess from "./FormSuccess"
+import Lines from "./Lines"
+import { contactSchema } from "./validationSchema"
+import { metrika, MetrikGoal } from "@/src/feature/metrika/MetrikSender"
+import Field from "../../ui/Field"
+import Textarea from "../../ui/Textarea"
+import Button from "../../ui/Button"
+import PhoneInput from "../../ui/PhoneInput"
 
 const urlLink =
-    'https://api.telegram.org/bot8170983542:AAF_mUheHm7MH6p5hzfxqiIwo0AzC1gJSAc/sendMessage?chat_id=-1002582530760&parse_mode=html&text=';
+  "https://api.telegram.org/bot8170983542:AAF_mUheHm7MH6p5hzfxqiIwo0AzC1gJSAc/sendMessage?chat_id=-1002582530760&parse_mode=html&text="
 
 /**
  * Форма обратной связи
  */
 const ContactForm: FC = () => {
-    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
-    const {
-        handleSubmit,
-        formState: { errors },
-        reset,
-        register,
-        control,
-    } = useForm<IContactData>({
-        defaultValues: {
-            name: '',
-            phone: '',
-            message: '',
-            email: '',
-        },
-        resolver: zodResolver(contactSchema),
-        mode: 'onChange',
-    });
+  const {
+    handleSubmit,
+    formState: { errors },
+    reset,
+    register,
+    control,
+  } = useForm<IContactData>({
+    defaultValues: {
+      name: "",
+      phone: "",
+      message: "",
+      email: "",
+    },
+    resolver: zodResolver(contactSchema),
+    mode: "onChange",
+  })
 
-    const errorMessages = Object.values(errors).map((error) => error.message);
+  const errorMessages = Object.values(errors).map((error) => error.message)
 
-    const onSubmit = async (data: IContactData) => {
-        const text = encodeURIComponent(`<b>Новая заявка с сайта:</b>
+  const onSubmit = async (data: IContactData) => {
+    const text = encodeURIComponent(`<b>Новая заявка с сайта:</b>
         <b>Имя:</b> ${data.name.trim()}
         <b>Телефон:</b> ${data.phone.trim()}
         <b>Почта:</b> ${data.email.trim()}
-        <b>Информация:</b> ${data.message ? data.message : 'отсутствует'}`);
+        <b>Информация:</b> ${data.message ? data.message : "отсутствует"}`)
 
-        await fetch(urlLink + text);
+    await fetch(urlLink + text)
 
-        metrika(MetrikGoal.SEND_FORM);
+    metrika(MetrikGoal.SEND_FORM)
 
-        setIsSuccess(true);
-    };
-    return (
-        <>
-            <AnimatePresence>
-                {!isSuccess && (
-                    <motion.form
-                        className="flex flex-col gap-5 pl-2 lg:pl-0 mt-6 lg:mt-7 lg:max-w-[650px] lg:mx-auto lg:gap-6  max-h-[532px]"
-                        onSubmit={handleSubmit(onSubmit)}
-                        initial={{
-                            opacity: 1,
-                        }}
-                        exit={{
-                            opacity: 0,
-                        }}
-                        transition={{
-                            duration: 0.5 + 1,
-                        }}>
-                        <Controller
-                            name="message"
-                            control={control}
-                            render={({ field }) => (
-                                <Textarea
-                                    {...field}
-                                    label="Готовы обсудить проект?"
-                                    rows={5}
-                                    className="tracking-wider"
-                                />
-                            )}
-                        />
+    setIsSuccess(true)
+  }
+  return (
+    <>
+      <AnimatePresence>
+        {!isSuccess && (
+          <motion.form
+            className="flex flex-col gap-5 pl-2 lg:pl-0 mt-6 lg:mt-7 lg:max-w-[650px] lg:mx-auto lg:gap-6  max-h-[532px] relative"
+            onSubmit={handleSubmit(onSubmit)}
+            initial={{
+              opacity: 1,
+            }}
+            exit={{
+              opacity: 0,
+            }}
+            transition={{
+              duration: 0.5 + 1,
+            }}
+          >
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => (
+                <Field
+                  {...field}
+                  label="Как вас зовут?"
+                  className=" px-3 py-2 tracking-wider"
+                  isValid={!errors.name?.message}
+                  error={errors.name?.message}
+                />
+              )}
+            />
+            <Controller
+              name="phone"
+              control={control}
+              render={({ field }) => (
+                <PhoneInput
+                  {...field}
+                  label="Ваш телефон"
+                  className=" px-3 py-2 tracking-wider"
+                  isValid={!errors.phone?.message}
+                  error={errors.phone?.message}
+                />
+              )}
+            />
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <Field
+                  {...field}
+                  label="Email"
+                  className=" px-3 py-2 tracking-wider"
+                  isValid={!errors.email?.message}
+                  error={errors.email?.message}
+                />
+              )}
+            />
 
-                        <Controller
-                            name="name"
-                            control={control}
-                            render={({ field }) => (
-                                <Field
-                                    {...field}
-                                    label="Как вас зовут?"
-                                    className=" px-3 py-2 tracking-wider"
-                                    isValid={!errors.name?.message}
-                                    error={errors.name?.message}
-                                />
-                            )}
-                        />
-                        <Controller
-                            name="phone"
-                            control={control}
-                            render={({ field }) => (
-                                <Field
-                                    {...field}
-                                    label="Ваш телефон"
-                                    className=" px-3 py-2 tracking-wider"
-                                    isValid={!errors.phone?.message}
-                                    error={errors.phone?.message}
-                                />
-                            )}
-                        />
-                        <Controller
-                            name="email"
-                            control={control}
-                            render={({ field }) => (
-                                <Field
-                                    {...field}
-                                    label="Email"
-                                    className=" px-3 py-2 tracking-wider"
-                                    isValid={!errors.email?.message}
-                                    error={errors.email?.message}
-                                />
-                            )}
-                        />
+            <Controller
+              name="message"
+              control={control}
+              render={({ field }) => (
+                <Textarea
+                  {...field}
+                  label="Пару слов о Вашем проекте?"
+                  rows={5}
+                  className="tracking-wider"
+                />
+              )}
+            />
 
-                        <Button
-                            className={`text-orange transition-all duration-300 ${
-                                errorMessages.length ? 'opacity-0' : ''
-                            }`}
-                            disabled={errorMessages.length > 0}
-                            type="submit">
-                            Отправить
-                        </Button>
-                    </motion.form>
-                )}
-            </AnimatePresence>
-            <AnimatePresence>
-                {isSuccess && (
-                    <>
-                        <Lines />
-                        <motion.div
-                            initial={{
-                                opacity: 0,
-                            }}
-                            animate={{
-                                opacity: 1,
-                            }}
-                            transition={{
-                                delay: 0.5 + 1.1,
-                                duration: 0.5,
-                            }}>
-                            <FormSuccess />
-                        </motion.div>
-                    </>
-                )}
-            </AnimatePresence>
-        </>
-    );
-};
+            <Button
+              className={`text-orange transition-all duration-300 ${
+                errorMessages.length ? "opacity-0" : ""
+              }`}
+              disabled={errorMessages.length > 0}
+              type="submit"
+            >
+              Отправить
+            </Button>
+          </motion.form>
+        )}
+      </AnimatePresence>
+      <AnimatePresence>
+        {isSuccess && (
+          <>
+            <Lines />
+            <motion.div
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{
+                delay: 0.5 + 1.1,
+                duration: 0.5,
+              }}
+            >
+              <FormSuccess />
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+    </>
+  )
+}
 
-export default ContactForm;
+export default ContactForm
