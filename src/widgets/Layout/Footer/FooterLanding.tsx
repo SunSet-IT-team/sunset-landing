0a69c53a020074297ac05954b94695012dcc5667
pan0mera <artess.ponomarev@mail.ru>
2026-02-25 11:15:31 +0300

    feat: glass footer landing

--- src/widgets/Layout/Footer/FooterLanding.tsx
+++ src/widgets/Layout/Footer/FooterLanding.tsx
@@ -5,6 +5,7 @@ import Legality from './Legality';
 import Questions from './Questions';
 import SocialMedia from './SocialMedia';
 import Container from '@/src/share/ui/Container';
+import { twMerge } from 'tailwind-merge';
 
 /**
  * Подвал сайта для лендинга интерактивный
@@ -49,26 +50,36 @@ const FooterLanding: FC = () => {
         <>
             {/* Триггер */}
             <div
-                className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full h-[12px] bg-blue-400 z-40 cursor-pointer"
+                className={twMerge(
+                    'fixed left-0 right-0 w-full h-[12px] z-40 cursor-pointer transition-all duration-300',
+                    isVisible ? '-bottom-10' : ' bottom-0',
+                )}
                 onMouseEnter={handleMouseEnter}
-                onClick={() => setIsVisible(true)}
-                style={{
-                    backgroundImage: `url('/icons/arrow.svg')`,
-                    backgroundSize: '10px 10px',
-                    backgroundPosition: 'center',
-                    backgroundRepeat: 'no-repeat',
-                }}
-            />
+                onClick={() => setIsVisible(true)}>
+                <div className="absolute inset-0  w-full  h-[12px] z-10 bg-blue-400 opacity-70"></div>
+                <div className="absolute inset-0  bg-opacity-50 backdrop-blur-md w-full  h-[12px] z-20"></div>
+                <div
+                    style={{
+                        backgroundImage: `url('/icons/arrow.svg')`,
+                        backgroundSize: '10px 10px',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                    }}
+                    className="relative z-50 mx-auto h-[12px] w-[12px]"></div>
+            </div>
 
             {/* Подвал с анимацией */}
             <footer
                 ref={footerRef}
-                className={`bg-blue-400 fixed left-0 right-0 bottom-0 py-2 md:py-4 h-26 md:h-36 z-50 transition-transform duration-300 ease-in-out ${
-                    isVisible ? 'translate-y-0' : 'translate-y-full'
-                }`}
+                className={twMerge(
+                    'fixed left-0 right-0 bottom-0 py-2 md:py-4 h-26 md:h-36 z-50 transition-transform duration-300 ease-in-out',
+                    isVisible ? 'translate-y-0' : 'translate-y-full',
+                )}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}>
-                <Container className="flex flex-col items-center justify-center gap-2 md:gap-4 w-full h-full relative">
+                <div className="fixed inset-0   w-full h-full z-10 bg-blue-400 opacity-40"></div>
+                <div className="fixed inset-0  bg-opacity-50 backdrop-blur-md w-full h-full z-20"></div>
+                <Container className="flex flex-col items-center justify-center gap-2 md:gap-4 w-full h-full relative z-[100]">
                     {/* <Questions /> */}
                     <SocialMedia />
                     <Legality />
